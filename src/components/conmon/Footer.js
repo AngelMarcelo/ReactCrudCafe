5df7d1625de2e8f742e45aab31edd3d08b4a0886
AngelMarcelo <ammarce@gmail.com>
2021-11-18 00:19:23 -0300

    se agrega codigo en navbar y footer

---
+++ src/components/conmon/Footer.js
@@ -0,0 +1,54 @@
+import  Footer from "react-bootstrap";
+
+import React from "react";
+import {
+Box,
+Container,
+Row,
+Column,
+FooterLink,
+Heading,
+} from "./FooterStyles";
+
+const Footer = () => {
+return (
+	<Box>
+	<h1 style={{ color: "green",
+				textAlign: "center",
+				marginTop: "-50px" }}>
+		Todos los derechos reservados
+	</h1>
+	<Container>
+		<Row>
+		
+		<Column>
+			<Heading>Social Media</Heading>
+			<FooterLink href="#">
+			<i className="fab fa-facebook-f">
+				<span style={{ marginLeft: "10px" }}>
+				Facebook
+				</span>
+			</i>
+			</FooterLink>
+			<FooterLink href="#">
+			<i className="fab fa-instagram">
+				<span style={{ marginLeft: "10px" }}>
+				Instagram
+				</span>
+			</i>
+			</FooterLink>
+			<FooterLink href="#">
+			<i className="fab fa-twitter">
+				<span style={{ marginLeft: "10px" }}>
+				Twitter
+				</span>
+			</i>
+			</FooterLink>
+			
+		</Column>
+		</Row>
+	</Container>
+	</Box>
+);
+};
+export default Footer;
---
+++ src/components/conmon/Navigation.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Navbar from "react-bootstrap";
+
+function Navbar() {
+    return (
+        <>
+            <Navbar bg="primary" variant="dark">
+                <Container>
+                    <Navbar.Brand href="#home">Crud Cafe</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Inicio</Nav.Link>
+                        <Nav.Link href="#features">Productos</Nav.Link>
+                        <Nav.Link href="#pricing">Extra</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+
+        </>
+    );
+}
